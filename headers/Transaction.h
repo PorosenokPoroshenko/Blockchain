b45e5c0b7bfea62ccddf1c5623be6a91d0d4089c
Yaroslav <fulkach@gmail.com>
2021-03-28 22:36:50 +0000

    Transaction struct added

---
+++ headers/Transaction.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+// class Transaction {
+// private:
+// std::string payer, payee, amount;
+
+// public:
+// Transaction(std::string payer, std::string payee, std::string amount);
+//};
+
+struct Transaction {
+    std::string payer, payee, amount;
+};
+
